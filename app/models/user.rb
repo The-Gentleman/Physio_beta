878071318b729f64c2e7fcb99b8ccf753a657824
The-Gentleman <zraja4402@gmail.com>
2020-10-04 13:32:42 -0400

    added users#create

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :exercises
     has_many :patients, through: :exercises
     has_secure_password
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true
 end
