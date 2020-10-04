efd91483a66d23ed3494e9aa2c4a5f72c475e13e
The-Gentleman <zraja4402@gmail.com>
2020-10-04 12:34:18 -0400

    added homepage and new form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    def home 
+    end 
+
+    def new 
+        @user = User.new
+    end 
+
+    def create 
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root "users#home"
+  get 'signup' => "users#new"
+  post 'signup' => "users#create"
+  get 'login' => "users#login"
+  post 'login' => "users#login"
+
   resources :patients
   resources :exercises
   resources :users
