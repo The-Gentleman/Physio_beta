9e841829feb2742b90f8a95c77369bd5d9f7825e
The-Gentleman <zraja4402@gmail.com>
2020-10-03 21:29:05 -0400

    AR relationships achieved.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,4 @@
 class Patient < ApplicationRecord
+    has_many :exercises
+    has_many :users, through: :exercises
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :exercises
+    has_many :patients, through: :exercises
 end
