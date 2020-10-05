a751073384fa637d5219f736f34ace5f4a971a8e
The-Gentleman <zraja4402@gmail.com>
2020-10-05 11:16:31 -0400

    added validations for exercise model

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,7 +1,8 @@
 class Exercise < ApplicationRecord
     belongs_to :user
     belongs_to :patient
-
+    validates :name, presence: true 
+    validates :reps, presence: true, numericality: true
     # TO DO:
     # CREATE VALIDATIONS FOR NAME AND REPS
 
