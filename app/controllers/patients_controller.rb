ac8eed2f78c89d135a445e04eaa6580b1915cc97
The-Gentleman <zraja4402@gmail.com>
2020-10-05 10:55:27 -0400

    added create functionality to patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,7 +3,17 @@ class PatientsController < ApplicationController
         @patient = Patient.new
     end 
 
+    def create 
+        @patient = Patient.create(patient_params)
+        redirect_to patients_path
+    end 
+
     def index 
         @patients = Patient.all
+    end 
+
+    private 
+    def patient_params
+        params.require(:patient).permit(:name, :diagnosis)
     end 
 end
