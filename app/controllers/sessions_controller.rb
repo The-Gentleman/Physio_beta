7e063da4536c4653a55648fadc26293df1efc28a
The-Gentleman <zraja4402@gmail.com>
2020-10-14 13:39:28 -0400

    added padding to center all views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
     def new 
     end 
-
+    
     def create
         @user = User.find_by(username: params[:user][:username])
         if @user && @user.authenticate(params[:user][:password])
